d08625a05e7f3bd4f8e793fcdc78d1356fcfc0c4
Azuwuigne Kizito <elochukwuazuwuigne@gmail.com>
2026-05-14 13:24:43 +0100

    Commit my code to Cse repo

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -5,5 +5,13 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Sandbox Project.");
+        Console.WriteLine("This is in C#.");
+
+        Console.WriteLine("What is your favorite color?  ");
+        string color = Console.ReadLine();
+        Console.WriteLine("Your favorite color is: " + color);
+        Console.Write(color);
+        Console.WriteLine($"{color} Wow that's a nice color!"); // String interpolation')
+
     }
 }
